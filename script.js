1378afc3b33bed8c1e8b4b634a4de9202e545b42
impronen <impronen@gmail.com>
2022-09-13 08:21:18 +0300

    OPERATE function
    Function for performing calculations based on operator input

---
+++ script.js
@@ -0,0 +1,34 @@
+// Global variables
+let result = "";
+
+//Basic functions
+function add(a, b) {return a + b}
+
+function subtract(a, b) {return a - b}
+
+function multiply(a, b) {return a * b}
+
+function divide(a, b) {return a / b}
+
+// Function to call calculations by operator
+function operate(operator){
+switch (operator) {
+    case "+":
+        console.log(operator);
+        result = add(a, b);
+        break;
+    case "-":
+        console.log(operator);
+        result = subtract(a, b);
+        break;
+    case "*":
+        console.log(operator);
+        result = multiply(a, b);
+        break;
+    case "/":
+        console.log(operator);
+        result = divide(a, b);
+        break;
+}
+return result
+}
